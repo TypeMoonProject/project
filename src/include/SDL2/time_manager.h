4386f81e09fc9b6c1e8ac082d0e33ea3dea698c6
WuChenmu <moore_wu@outlook.com>
2025-12-03 19:42:04 +0800

    Add files via upload

---
+++ src/include/SDL2/time_manager.h
@@ -0,0 +1,29 @@
+#ifndef TIME_MANAGER_H
+#define TIME_MANAGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "core_struct.h"
+#define MAX_TIMER 20 //暂定最多共20个计时器
+typedef struct{
+    int id; //计时器编号,0是总计时器，后续是顾客,和后续数组对应
+    int initial_time;
+    int present_time;
+    int duration;
+    int remaining; //剩余时间
+    bool is_active; //是否激活
+}Timer;
+
+#endif
+
+/*示例
+计时器需要在.c中启用
+while (1){
+    timers[0].initial_time = time();
+    timers[0].present_time = time();
+    timers[0].remaining = timers[0].duration + timers[0].initial_time - timers[0].present_time;
+    }
+0是总计时器，后续是顾客
+根据需要修改
+*/
