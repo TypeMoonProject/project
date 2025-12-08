ee08b8b8fd7df43b2a319771c1b06c194576dd55
TypeMoonProject <2466289356@qq.com>
2025-12-08 16:24:34 +0800

    modified:   main.c 	new file:   sourse/music/money.wav 	new file:   sourse/picture/begin.png 	new file:   src/include/SDL2/SDL_mixer.h 	modified:   src/include/SDL2/ui.h 	modified:   ui.c

--- src/include/SDL2/ui.h
+++ src/include/SDL2/ui.h
@@ -1,19 +1,50 @@
 #ifndef UI
 #define UI
 
-#include<stdio.h>
-#include<string.h>
-#include<time.h>
-#include<stdlib.h>
 #include<SDL.h>
 #include<SDL_ttf.h>
+#include<SDL_image.h>
+#include<SDL_mixer.h>
+#include<stdbool.h>
+#include<stdio.h>
 
-//初始化界面(成功返回0，失败返回-1)
-int init_ui();
+typedef struct color{
+    int r;
+    int g;
+    int b;
+    int a;
+}color;
 
-//创建窗口
-SDL_Window *creat_ui();
+char position[]="./sourse/picture/begin.png";
+//(X,Y)起始点(W,H)区域宽高
+//                 X   Y   H   W
+SDL_Rect rect[]={{200,200,10 ,10 },//鼠标
+                 {750,500,200,50 },//封面按键
+             };
+//            R   G   B   A
+color col[]={{0  ,0  ,0  ,255},//黑
+             {255,0  ,0  ,255},//红
+             {0  ,255,0  ,255},//绿
+             {0  ,0  ,255,255},//蓝
+             {255,255,255,255},//白
+             {0  ,0  ,0  ,50 },//半透明
+        };
+int speed=10;
+char begin[]="begin";
 
-//退出界面并销毁窗口(输入creat_ui的返回值)
-void quit_ui(SDL_Window *window);
-#endif;
+SDL_Window* init_creat();
+TTF_Font *open_font(char *font_position,int size);
+void print(TTF_Font *font,char *word,color *col,SDL_Renderer *ren,SDL_Rect *rect);
+SDL_Renderer *create_renderer(SDL_Window *win);
+SDL_Surface *load_image(char *position);
+void draw_rectangle(SDL_Renderer *ren,SDL_Rect *rect,color *col);
+void callback(void *userdata,Uint8 *stream,int len);
+void back_music(int id);
+void into_image(SDL_Window *win,SDL_Renderer *ren,SDL_Surface *ima);
+void move_rect(SDL_Renderer *ren,SDL_Rect *rect,color *col,int speed);
+void quit(SDL_Window *win,SDL_Renderer *ren);
+void draw_ui(SDL_Renderer *ren);
+void action(char *position,SDL_Rect *rect,color *col,int speed);
+void first_ui(SDL_Renderer *ren,SDL_Window *win,SDL_Rect *rect,color *col1,color *col2,char *position,char *f_p,int size);
+
+#endif
--- ui.c
+++ ui.c
@@ -1,41 +1,231 @@
-#include<stdio.h>
-#include<string.h>
-#include<time.h>
-#include<stdlib.h>
 #include<SDL.h>
 #include<SDL_ttf.h>
+#include<SDL_image.h>
+#include<stdbool.h>
+#include<stdio.h>
 
-//初始化界面(成功返回0，失败返回-1)
-int init_ui(){
-    if(SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO)){
-        SDL_Log("init_ui failed:%s\n",SDL_GetError);
-        return -1;
+const int WIDTH=1000;
+const int HIGHT=750;
+Uint8 *buf=NULL;
+Uint32 audio_len;
+Uint32 pos=0;
+SDL_AudioDeviceID id;
+char b[]="begin";
+char f_p[]="./font/Arial.ttf";
+
+typedef struct color{
+    int r;
+    int g;
+    int b;
+    int a;
+}color;
+
+//初始化和创建窗口
+SDL_Window* init_creat(){
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)!=0){
+        SDL_Log("INIT UI FAILED:%s",SDL_GetError());
     }
-    return 0;
-}
-
-//创建窗口
-SDL_Window *creat_ui(){
-    const int width=1000;
-    const int hight=1200;
-    char title[]="Saweima Legend";
-    SDL_Window *window=SDL_CreateWindow(
-                                title,                 //窗口标题
-                                SDL_WINDOWPOS_CENTERED,//居中
-                                SDL_WINDOWPOS_CENTERED,//居中
-                                width,                 //窗口宽度
-                                hight,                 //窗口高度
-                                0                      //flag
+    if(TTF_Init()<0){
+        SDL_Log("INIT TTF FAILED:%s",TTF_GetError());
+    }
+    SDL_Window *win=SDL_CreateWindow("game",
+                                    SDL_WINDOWPOS_CENTERED,//x轴居中
+                                    SDL_WINDOWPOS_CENTERED,//y轴居中
+                                    WIDTH,                 //宽
+                                    HIGHT,                 //高
+                                    SDL_WINDOW_RESIZABLE   //大小可变
                                 );
-    //检验是否成功
-    if(window==NULL){
-        SDL_Log("creat_ui failed:%s\n",SDL_GetError);
+    if(win==NULL){
+        SDL_Log("CREATE UI FAILED:%s",SDL_GetError());
     }
-        return window;
+    return win;
 }
 
-//退出界面并销毁窗口(输入creat_ui的返回值)
-void quit_ui(SDL_Window *window){
-    SDL_DestroyWindow(window);
-    SDL_QUIT;
+//打开字体
+TTF_Font *open_font(char *font_position,int size){
+    TTF_Font *font=TTF_OpenFont(font_position,size);
+    if(font==NULL){
+        SDL_Log("%s",TTF_GetError());
+    }
+    return font;
+}
+
+//渲染文字
+void print(TTF_Font *font,char *word,color *col,SDL_Renderer *ren,SDL_Rect *rect){
+    SDL_Color co={col->r,col->g,col->b,col->a};
+    SDL_Surface *wo=TTF_RenderUTF8_Blended(font,word,co);
+    SDL_Texture* text=SDL_CreateTextureFromSurface(ren,wo);
+    SDL_FreeSurface(wo);
+    SDL_RenderCopy(ren,text,NULL,rect);
+}
+
+
+//创建渲染器
+SDL_Renderer *create_renderer(SDL_Window *win){
+    SDL_Renderer *ren=SDL_CreateRenderer(win,                   //创建渲染器的窗口
+                                        -1,                     //自动寻找
+                                        SDL_RENDERER_ACCELERATED//使用硬件加速
+                                    );
+    if(ren==NULL){
+        SDL_Log("CREATE RENDERER FAILED:%s",SDL_GetError());
+    }
+    //设置混合模式
+    SDL_SetRenderDrawBlendMode(ren,SDL_BLENDMODE_BLEND);
+    return ren;
+}
+
+//下载图片
+SDL_Surface *load_image(char *position){
+    SDL_Surface *ima=IMG_Load(position);
+    if(ima==NULL){
+        SDL_Log("LOAD JPG FAILED:%s",SDL_GetError());
+    }
+    return ima;
+}
+
+//导入图片至渲染器
+ void into_image(SDL_Window *win,SDL_Renderer *ren,SDL_Surface *ima){
+    IMG_Init(IMG_INIT_PNG);
+    SDL_Texture *text=SDL_CreateTextureFromSurface(ren,ima);
+    SDL_FreeSurface(ima);
+    if(text==NULL){
+        SDL_Log("CREATE TEXTURE FAILED:%s",SDL_GetError());
+    }
+    //导入渲染器
+    SDL_RenderCopy(ren,
+                text,
+                NULL,//复制图片的区域
+                NULL //目标区域
+                );
+    SDL_DestroyTexture(text);
+}
+
+//绘制矩形
+void draw_rectangle(SDL_Renderer *ren,SDL_Rect *rect,color *col){
+    SDL_SetRenderDrawColor(ren,col->r,col->g,col->b,col->a);
+    SDL_RenderFillRect(ren,rect);
+}
+
+//回调
+void callback(void *userdata,Uint8 *stream,int len){
+    int remain=audio_len-pos;
+    if(remain>len){
+        SDL_memcpy(stream,buf+pos,len);
+        pos+=len;
+    }else{
+        SDL_memcpy(stream,buf+pos,remain);
+        pos=0;
+    }
+}
+
+//播放背景音乐
+void back_music(int id){
+    SDL_AudioSpec spec;
+    if(SDL_LoadWAV("./sourse/music/money.wav",&spec,&buf,&audio_len)==NULL){
+        SDL_Log("LOAD MUSIC FAILED:%s",SDL_GetError());
+    }
+    spec.callback=callback;
+    id=SDL_OpenAudioDevice(NULL,//默认设备
+                        0,      //播放
+                        &spec,
+                        NULL,
+                        0
+                    );
+    if(id==0){
+        SDL_Log("OPEN AUDIO FAILED:%s",SDL_GetError());
+    }
+    SDL_PauseAudioDevice(id,0);
+}
+
+//使矩形沿x轴移动（需搭配循环使用）
+void move_rect(SDL_Renderer *ren,SDL_Rect *rect,color *col,int speed){
+    draw_rectangle(ren,rect,col);
+    rect->x+=speed;
+    SDL_Delay(100);
+    SDL_RenderPresent(ren);
+}
+
+//退出
+void quit(SDL_Window *win,SDL_Renderer *ren){
+    SDL_DestroyRenderer(ren);
+    SDL_CloseAudioDevice(id);
+    SDL_FreeWAV(buf);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    IMG_Quit();
+    SDL_Quit();
+}
+
+//绘制界面
+void draw_ui(SDL_Renderer *ren){
+    SDL_RenderPresent(ren);
+}
+
+//开始界面
+void first_ui(SDL_Renderer *ren,SDL_Window *win,SDL_Rect *rect,color *col1,color *col2,char *position,char *f_p,int size){
+    SDL_Surface *ima=load_image(position);
+    into_image(win,ren,ima);
+    draw_rectangle(ren,rect,col1);
+    TTF_Font *font=open_font(f_p,size);
+    print(font,b,col2,ren,rect);
+}
+
+//游戏界面
+void game_ui(SDL_Renderer *ren,SDL_Window *win,SDL_Rect *rect,color *col1,color *col2,char *position,char *f_p,int size){
+}
+
+//监听行为
+void action(char *position,SDL_Rect *rect,color *col,int speed){
+    SDL_Window *win=init_creat();
+    SDL_Renderer *ren=create_renderer(win);
+    SDL_Event event;
+    first_ui(ren,win,&rect[1],&col[0],&col[4],position,f_p,10);
+    draw_ui(ren);
+    //back_music(id);
+    bool run=true;
+    while(run){
+        while(SDL_PollEvent(&event)!=0){
+            switch(event.type){
+                case SDL_QUIT:                            //退出事件
+                    run=false;
+                    break;
+                case SDL_WINDOWEVENT:                     //窗口事件
+                    switch(event.window.event){
+                        case SDL_WINDOWEVENT_SIZE_CHANGED://窗口大小变化事件
+                            //窗口大小变化后，原有纹理失效
+                            first_ui(ren,win,&rect[1],&col[0],&col[4],position,f_p,72);
+                            draw_ui(ren);
+                            break;
+                    }break;
+                case SDL_MOUSEMOTION:                     //鼠标移动事件
+                    //矩形跟随鼠标
+                    rect[0].x=event.motion.x-5;
+                    rect[0].y=event.motion.y-5;
+                    first_ui(ren,win,&rect[1],&col[0],&col[4],position,f_p,72);
+                    draw_rectangle(ren,&rect[0],&col[0]);
+                    draw_ui(ren);
+                    break;
+                case SDL_MOUSEBUTTONDOWN:                 //鼠标按下事件
+                    draw_rectangle(ren,&rect[0],&col[4]);
+                    draw_ui(ren);
+                    break;
+                case SDL_MOUSEBUTTONUP:                   //鼠标抬起事件
+                    draw_rectangle(ren,&rect[0],&col[0]);
+                    draw_ui(ren);
+                    break;
+                case SDL_KEYDOWN:                         //按键按下事件
+                    switch(event.key.keysym.sym){
+                        //按esc退出
+                        case SDLK_ESCAPE:                 //按下esc
+                            goto QUIT;
+                            break;
+                    }
+                    break;
+                case SDL_KEYUP:                           //案件抬起事件
+                    break;
+            }
+        }
+    }
+    //quit必须放在事件循环外
+    QUIT: quit(win,ren);
 }
