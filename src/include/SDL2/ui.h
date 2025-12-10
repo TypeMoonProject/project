5efce7e53307d0e7826929856de028b5e5f281c7
TypeMoonProject <2466289356@qq.com>
2025-12-10 17:19:53 +0800

    new file:   sourse/picture/data.jpg 	new file:   sourse/picture/data.png 	new file:   sourse/picture/head.png 	modified:   src/include/SDL2/ui.h 	modified:   ui.c

--- src/include/SDL2/ui.h
+++ src/include/SDL2/ui.h
@@ -7,6 +7,7 @@
 #include<SDL_mixer.h>
 #include<stdbool.h>
 #include<stdio.h>
+#include<core_struct.h>
 
 typedef struct color{
     int r;
@@ -18,9 +19,19 @@ typedef struct color{
 char position[]="./sourse/picture/begin.png";
 char f_p[]="./font/Arial.ttf";
 //(X,Y)起始点(W,H)区域宽高
-//                 X   Y   H   W
+//                 X   Y   W   H
 SDL_Rect rect[]={{200,200,10 ,10 },//鼠标
                  {750,500,200,50 },//封面按键
+                 {0  ,210,76 ,76 },
+                 {76 ,210,76 ,76 },
+                 {152,210,76 ,76 },
+                 {228,210,76 ,76 },
+                 {304,210,76 ,76 },
+                 {380,210,76 ,76 },
+                 {456,210,76 ,76 },
+                 {532,210,76 ,76 },
+                 {608,210,76 ,76 },
+                 {684,210,76 ,76 },
              };
 //            R   G   B   A
 color col[]={{0  ,0  ,0  ,255},//黑
@@ -46,5 +57,9 @@ void move_rect(SDL_Renderer *ren,SDL_Rect *rect,color *col,int speed);
 void quit(SDL_Window *win,SDL_Renderer *ren);
 void draw_ui(SDL_Renderer *ren);
 void first_ui(SDL_Renderer *ren,SDL_Window *win,SDL_Rect *rect,color *col1,color *col2,char *position,char *f_p,int size);
+void game_ui(SDL_Renderer *ren,SDL_Window *win,char *f_p,Inventory *inventory,Customer *customer);
+void print_number(TTF_Font *font,Inventory *num,color *col,SDL_Renderer *ren,SDL_Rect *rect);
+void print_req(TTF_Font *font,Customer *customer,color *col,SDL_Renderer *ren);
+
 
 #endif
--- ui.c
+++ ui.c
@@ -3,6 +3,7 @@
 #include<SDL_image.h>
 #include<stdbool.h>
 #include<stdio.h>
+#include<core_struct.h>
 
 const int WIDTH=1000;
 const int HIGHT=750;
@@ -174,6 +175,105 @@ void first_ui(SDL_Renderer *ren,SDL_Window *win,SDL_Rect *rect,color *col1,color
 }
 
 //游戏界面
-void game_ui(SDL_Renderer *ren,SDL_Window *win,SDL_Rect *rect,color *col1,color *col2,char *position,char *f_p,int size){
+void game_ui(SDL_Renderer *ren,SDL_Window *win,char *f_p,Inventory *inventory,Customer *customer){
+    char g_p[]="./sourse/picture/kitchen.png";
+    char g_p1[]="./sourse/picture/data.png";
+    char g_p2[]="./sourse/picture/head.png";
+    SDL_Surface *ima =load_image(g_p );
+    SDL_Surface *ima1=load_image(g_p1);
+    SDL_Surface *ima2=load_image(g_p2);
+    into_image(win,ren,ima);
+    SDL_Texture *text=SDL_CreateTextureFromSurface(ren,ima1);
+    SDL_FreeSurface(ima1);
+    if(text==NULL){
+        SDL_Log("CREATE TEXTURE FAILED:%s",SDL_GetError());
+    }
+    SDL_Rect re1={0,0,210,760};
+    SDL_Rect re2={439,500,61,100};
+    //导入渲染器
+    SDL_RenderCopy(ren,
+                text,
+                NULL,//复制图片的区域
+                &re1 //目标区域
+                );
+    SDL_DestroyTexture(text);
+    SDL_Texture *text1=SDL_CreateTextureFromSurface(ren,ima);
+    SDL_FreeSurface(ima);
+    if(text==NULL){
+        SDL_Log("CREATE TEXTURE FAILED:%s",SDL_GetError());
+    }
+    //导入渲染器
+    SDL_RenderCopy(ren,
+                text,
+                NULL,//复制图片的区域
+                &re2 //目标区域
+                );
+    SDL_DestroyTexture(text1);
+}
+//打印数字
+void print_number(TTF_Font *font,Inventory *num,color *col,SDL_Renderer *ren,SDL_Rect *rect){
+    char text[10][4]={'\0'};
+    sprintf(text[0],"%d",num->flatbread);
+    sprintf(text[1],"%d",num->meat);
+    sprintf(text[2],"%d",num->cucumber);
+    sprintf(text[3],"%d",num->sauce);
+    sprintf(text[4],"%d",num->chips);
+    sprintf(text[5],"%d",num->ketchup);
+    sprintf(text[6],"%d",num->cola);
+    sprintf(text[7],"%d",num->wrapper);
+    sprintf(text[8],"%d",num->box);
+    sprintf(text[9],"%d",num->bottle);
+    print(font,text[0],col,ren,&rect[2]);
+    print(font,text[1],col,ren,&rect[3]);
+    print(font,text[2],col,ren,&rect[4]);
+    print(font,text[3],col,ren,&rect[5]);
+    print(font,text[4],col,ren,&rect[6]);
+    print(font,text[5],col,ren,&rect[7]);
+    print(font,text[6],col,ren,&rect[8]);
+    print(font,text[7],col,ren,&rect[9]);
+    print(font,text[8],col,ren,&rect[10]);
+    print(font,text[9],col,ren,&rect[11]);
 }
 
+//打印要求
+void print_req(TTF_Font *font,Customer *customer,color *col,SDL_Renderer *ren){
+    // 设置表格参数
+    int startX=500,startY=500;    // 起始坐标
+    int cellWidth=20,cellHeight=20; // 单元格尺寸
+    int padding=5;                  // 文本内边距
+    //
+    char req[5][5]={'\0'};
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            if(((customer->pancake_requirement)[i][j])==1){
+                req[i][j]='y';
+            }else{
+                req[i][i]='n';
+            }
+        }
+    }
+    //
+    for (int row=0;row<5;row++) {
+        for (int col=0;col<5;col++) {
+            SDL_Rect cellRect = {startX + col*cellWidth, 
+                            startY + row*cellHeight,
+                            cellWidth, cellHeight};
+            SDL_SetRenderDrawColor(ren, 240, 240, 240, 255); // 浅灰背景
+            SDL_RenderFillRect(ren, &cellRect);
+            SDL_SetRenderDrawColor(ren, 0, 0, 0, 255); // 黑色边框
+            SDL_RenderDrawRect(ren, &cellRect);
+            SDL_Color textColor = {0, 0, 0, 255}; // 黑色文本
+            SDL_Surface* textSurface = TTF_RenderText_Blended(font,&req[row][col], textColor);
+            SDL_Texture* textTexture = SDL_CreateTextureFromSurface(ren, textSurface);
+            SDL_Rect textRect = {
+                cellRect.x + (cellWidth - textSurface->w)/2,
+                cellRect.y + (cellHeight - textSurface->h)/2,
+                textSurface->w,
+                textSurface->h
+            };
+            SDL_RenderCopy(ren, textTexture, NULL, &textRect);
+            SDL_FreeSurface(textSurface);
+            SDL_DestroyTexture(textTexture);
+        }
+    }
+}
